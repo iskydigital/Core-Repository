85df66c9b4dc86e9c1da6c141be546390d003e47
iskydigital <admin@isky.digital>
2026-04-05 14:37:36 +0000

    chore: remove payload-test debugging folder from repo

--- payload-test/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from 'next'
-import { withPayload } from '@payloadcms/next/withPayload'
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['31.97.148.228'],
-}
-
-export default withPayload(nextConfig)
